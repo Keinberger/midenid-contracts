b907715bb58c49e5d07345c80dbc97378c64144d
Bora <bora_atik98@hotmail.com>
2025-12-11 19:25:17 +0300

    random serial number added to notes, note tag is changed, naming account converted to network type.

--- src/notes.rs
+++ src/notes.rs
@@ -1,16 +1,34 @@
-use miden_assembly::{DefaultSourceManager, Library, LibraryPath, ast::{Module, ModuleKind}};
+use miden_assembly::{
+    DefaultSourceManager, Library, LibraryPath,
+    ast::{Module, ModuleKind},
+};
 use miden_client::{
-    Client, ScriptBuilder, account::{AccountBuilder, AccountId, AccountStorageMode, AccountType}, auth::NoAuth, keystore::FilesystemKeyStore, note::{Note, NoteAssets, NoteExecutionHint, NoteInputs, NoteMetadata, NoteRecipient, NoteTag, NoteType}, transaction::TransactionKernel
+    Client, ScriptBuilder,
+    account::{AccountBuilder, AccountId, AccountStorageMode, AccountType},
+    auth::NoAuth,
+    keystore::FilesystemKeyStore,
+    note::{
+        Note, NoteAssets, NoteExecutionHint, NoteExecutionMode, NoteInputs, NoteMetadata,
+        NoteRecipient, NoteTag, NoteType,
+    },
+    transaction::TransactionKernel,
 };
 use miden_crypto::{Felt, Word};
 use miden_objects::account::AccountComponent;
-use rand::{RngCore, rngs::StdRng};
+use rand::{Rng, RngCore, rngs::StdRng};
 use std::{fs, path::Path, sync::Arc};
 
-pub async fn create_note_for_naming(name: String, inputs: NoteInputs, sender: AccountId, target_id: AccountId, assets: NoteAssets) -> anyhow::Result<Note> {
+pub async fn create_note_for_naming(
+    name: String,
+    inputs: NoteInputs,
+    sender: AccountId,
+    target_id: AccountId,
+    assets: NoteAssets,
+) -> anyhow::Result<Note> {
     let note_code = fs::read_to_string(Path::new(&format!("./masm/notes/{}.masm", name)))?;
     let naming_code = fs::read_to_string(Path::new("./masm/accounts/naming.masm")).unwrap();
     let library = create_library(naming_code, "miden_name::naming")?;
+    let serial = generate_random_serial_number();
 
     let note_script = ScriptBuilder::new(true)
         .with_dynamically_linked_library(&library)
@@ -18,9 +36,15 @@ pub async fn create_note_for_naming(name: String, inputs: NoteInputs, sender: Ac
         .compile_note_script(note_code)
         .unwrap();
 
-    let recipient = NoteRecipient::new(Word::default(), note_script, inputs.clone());
-    let tag = NoteTag::from_account_id(target_id);
-    let metadata = NoteMetadata::new(sender, NoteType::Public, tag, NoteExecutionHint::none(), Felt::new(0))?;
+    let recipient = NoteRecipient::new(serial, note_script, inputs.clone());
+    let tag = NoteTag::for_public_use_case(0, 0, NoteExecutionMode::Local).unwrap();
+    let metadata = NoteMetadata::new(
+        sender,
+        NoteType::Public,
+        tag,
+        NoteExecutionHint::always(),
+        Felt::new(0),
+    )?;
     let note = Note::new(assets, metadata, recipient);
     Ok(note)
 }
@@ -28,12 +52,29 @@ pub async fn create_note_for_naming(name: String, inputs: NoteInputs, sender: Ac
 pub fn create_library(account_code: String, library_path: &str) -> anyhow::Result<Library> {
     let assembler = TransactionKernel::assembler().with_debug_mode(true);
     let source_manager = Arc::new(DefaultSourceManager::default());
-    let module = Module::parser(ModuleKind::Library).parse_str(
-        LibraryPath::new(library_path)?,
-        account_code,
-        &source_manager,
-    ).unwrap();
+    let module = Module::parser(ModuleKind::Library)
+        .parse_str(
+            LibraryPath::new(library_path)?,
+            account_code,
+            &source_manager,
+        )
+        .unwrap();
     let library = assembler.clone().assemble_library([module]).unwrap();
 
     Ok(library)
+}
+
+/// Generates a random serial number for note creation
+///
+/// Similar to TypeScript's generateRandomSerialNumber()
+/// Returns a Word containing 4 random Felt values
+pub fn generate_random_serial_number() -> Word {
+    let mut rng = rand::rng();
+
+    Word::new([
+        Felt::new(rng.random::<u32>() as u64),
+        Felt::new(rng.random::<u32>() as u64),
+        Felt::new(rng.random::<u32>() as u64),
+        Felt::new(rng.random::<u32>() as u64),
+    ])
 }
